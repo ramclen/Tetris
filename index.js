6fb6184189e54fe8d17701669027d2b1a0c8f940
Cristopher <cristopher.correo@gmail.com>
2020-05-09 15:27:55 +0100

    create collitions

--- index.js
+++ index.js
@@ -10,10 +10,33 @@ const matrix = [
   [0, 1, 0],
 ]
 
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 &&
+        (arena[y + o.y] &&
+          arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function createMatrix(w, h) {
+  const matrix = [];
+  while (h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
 function draw() {
   context.fillStyle = "#000";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
+  drawMatrix(arena, { x: 0, y: 0 })
   drawMatrix(player.matrix, player.pos)
 }
 
@@ -48,16 +71,44 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+function playerDrop() {
+  player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
+}
+
+function playerMove(dir) {
+  player.pos.x += dir;
+  if (collide(arena, player)) {
+    player.pos.x -= dir;
+  }
+}
+
 document.addEventListener('keydown', event => {
   if (event.keyCode === 37) {
-    player.pos.x--;
+    playerMove(-1)
   } else if (event.keyCode === 39) {
-    player.pos.x++;
+    playerMove(1)
   } else if (event.keyCode === 40) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerDrop();
   }
 })
+
+const arena = createMatrix(12, 20);
+
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value
+      }
+    })
+  })
+}
 
 const player = {
   pos: { x: 5, y: 5 },
