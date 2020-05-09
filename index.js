d64e6bef1b695172a8da4d5b0110cefbb57f3478
Cristopher <cristopher.correo@gmail.com>
2020-05-09 14:50:33 +0100

    drawing updates and listening player inputs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('tetris')
 const context = canvas.getContext('2d');
 
-context.fillStyle = "#000";
-context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+context.scale(20, 20)
+
 
 const matrix = [
   [0, 0, 0],
@@ -10,11 +10,58 @@ const matrix = [
   [0, 1, 0],
 ]
 
-matrix.forEach((row, y) => {
-  row.forEach((value, x) => {
-    if (value !== 0) {
-      context.fillStyle = 'red';
-      context.fillRect(x, y, 1, 1);
-    }
+function draw() {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  drawMatrix(player.matrix, player.pos)
+}
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle = 'red';
+        context.fillRect(
+          x + offset.x,
+          y + offset.y,
+          1, 1
+        );
+      }
+    })
   })
+}
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    player.pos.y++;
+    dropCounter = 0;
+  }
+  draw();
+  requestAnimationFrame(update);
+}
+
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 37) {
+    player.pos.x--;
+  } else if (event.keyCode === 39) {
+    player.pos.x++;
+  } else if (event.keyCode === 40) {
+    player.pos.y++;
+    dropCounter = 0;
+  }
 })
+
+const player = {
+  pos: { x: 5, y: 5 },
+  matrix
+}
+
+update();
